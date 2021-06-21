b6ebecbac27707639de266dc69df27c352bd145d
Anthony Costanza <anthonycost99@gmail.com>
2021-06-21 12:48:43 -0700

    created group and round models but not properly referencing other tables

---
+++ backend/db/migrations/20210621194712-create-round.js
@@ -0,0 +1,43 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Rounds", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      courseId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      groupId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      ruleset: {
+        type: Sequelize.STRING,
+      },
+      startTime: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      holes: {
+        allowNull: false,
+        type: Sequelize.NUMERIC,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Rounds");
+  },
+};
