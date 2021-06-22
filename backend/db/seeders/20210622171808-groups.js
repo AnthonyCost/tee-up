295bd4c83c19ee7d1ca79a7d1c4ace4f3c0d4727
Anthony Costanza <anthonycost99@gmail.com>
2021-06-22 10:28:57 -0700

    added seed data for groups but have yet to seed database

--- backend/db/seeders/20210622171808-groups.js
+++ backend/db/seeders/20210622171808-groups.js
@@ -1,22 +1,27 @@
 "use strict";
 
-// {
-//           hostUserId: DataTypes.INTEGER,
-//           playStyle: DataTypes.STRING,
-//           description: DataTypes.TEXT,
-//           groupName: DataTypes.STRING,
-//         },
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert(
       "Groups",
       [
         {
-          hostUserId: ,
-          playStyle: "",
-          description: "",
-          groupName: "",
+          hostUserId: 1,
+          playStyle: "Casual",
+          description: "Still learning!",
+          groupName: "Beginners here!",
+        },
+        {
+          hostUserId: 2,
+          playStyle: "Professional",
+          description: "We are a group of PGA level players",
+          groupName: "The Pros",
+        },
+        {
+          hostUserId: 3,
+          playStyle: "Competitive",
+          description: "Playing for money!",
+          groupName: "Gamblers",
         },
       ],
       {}
