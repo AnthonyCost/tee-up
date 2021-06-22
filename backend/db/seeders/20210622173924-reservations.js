7e4dd91a020a320e18fb3b77bc8abac7fa4a4e3e
Anthony Costanza <anthonycost99@gmail.com>
2021-06-22 10:45:59 -0700

    added seeder data to reservations but not yet seeded

--- backend/db/seeders/20210622173924-reservations.js
+++ backend/db/seeders/20210622173924-reservations.js
@@ -1,17 +1,40 @@
-'use strict';
+"use strict";
+
+// userId: DataTypes.INTEGER,
+// status: DataTypes.BOOLEAN,
+// roundId: DataTypes.INTEGER,
+// createdAt: new Date(),
+// updatedAt: new Date(),
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert(
+      "Reservations",
+      [
+        {
+          userId: 1,
+          status: false,
+          roundId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 3,
+          status: true,
+          roundId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          userId: 2,
+          status: true,
+          roundId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +43,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+      */
+    return queryInterface.bulkDelete("Reservations", null, {});
+  },
 };
