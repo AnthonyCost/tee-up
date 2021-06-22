2496490194a919ce5116a86f95cdc02e31d9cda6
Anthony Costanza <anthonycost99@gmail.com>
2021-06-22 16:29:12 -0700

    setting up the express route for groups

---
+++ backend/routes/api/groups.js
@@ -0,0 +1,9 @@
+// create router here
+// since we're doing db stuff, you'll want some kind of asyncHandler
+
+// Take a second to import the database stuff you'll need
+// Here's where you'd also import the other middleware
+
+// create the api route
+
+// remember to export the router too
