585436ecd33eb3e25bb0bef9d2531cf281bcdcd0
Anthony Costanza <anthonycost99@gmail.com>
2021-06-23 11:05:49 -0700

    have the basic template for the unstyled card

--- frontend/src/components/GroupCard/GroupCard.js
+++ frontend/src/components/GroupCard/GroupCard.js
@@ -1,12 +1,7 @@
 const GroupCard = ({ group }) => {
   return (
-    <tr>
-      <td>{group.id}</td>
-      <td>{group.hostUserId}</td>
-      <td>{group.playStyle}</td>
-      <td>{group.description}</td>
-      <td>{group.groupName}</td>
-      <td>
+    <div className="card-container">
+      <div className="image-container">
         <img
           src={
             group.imageUrl
@@ -14,8 +9,37 @@ const GroupCard = ({ group }) => {
               : "https://images.unsplash.com/photo-1612672916367-a185107c8aaa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
           }
         />
-      </td>
-    </tr>
+      </div>
+      <div className="card-title">
+        <h2>{group.groupName}</h2>
+      </div>
+      <div className="card-subTitle">
+        <h3>Hosted by: {group.hostUserId}</h3>
+      </div>
+      <div className="card-description">
+        <p>{group.description}</p>
+      </div>
+      <div className="card-button">
+        <a>More Info</a>
+      </div>
+    </div>
+
+    // <tr>
+    //   <td>{group.id}</td>
+    //   <td>{group.hostUserId}</td>
+    //   <td>{group.playStyle}</td>
+    //   <td>{group.description}</td>
+    //   <td>{group.groupName}</td>
+    //   <td>
+    //     <img
+    //       src={
+    //         group.imageUrl
+    //           ? group.imageUrl
+    //           : "https://images.unsplash.com/photo-1612672916367-a185107c8aaa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
+    //       }
+    //     />
+    //   </td>
+    // </tr>
   );
 };
 
