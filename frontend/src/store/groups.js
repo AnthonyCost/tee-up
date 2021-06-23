20623b5319c2d6b9dd7829f5567fe44a735547b7
Anthony Costanza <anthonycost99@gmail.com>
2021-06-22 22:17:45 -0700

    added groups store

---
+++ frontend/src/store/groups.js
@@ -0,0 +1,37 @@
+// Define Action Types as Constants
+const SET_GROUPS = "groups/SET_GROUPS";
+
+// Define Action Creators
+const setGroups = (groups) => ({
+  type: SET_GROUPS,
+  groups,
+});
+
+// Define Thunks
+export const getGroups = () => async (dispatch) => {
+  const res = await csrfFetch("/api/groups");
+  const groups = await res.json();
+  dispatch(setGroups(groups));
+};
+
+// Define an initial state
+const initialState = {};
+
+// Define a reducer
+const groupsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_GROUPS:
+      const allGroups = {};
+      action.groups.forEach((group) => {
+        allGroups[group.id] = group;
+      });
+      return {
+        ...state,
+        ...allGroups,
+      };
+    default:
+      return state;
+  }
+};
+// Export the reducer
+export default groupsReducer;
