a488f7806e5caa4dc32c86a4b6d99583042de602
Anthony Costanza <anthonycost99@gmail.com>
2021-06-24 10:41:06 -0700

    added frontEnd create group form

--- frontend/src/components/CreateGroupForm/CreateGroupForm.js
+++ frontend/src/components/CreateGroupForm/CreateGroupForm.js
@@ -1,1 +1,95 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGroups, createGroup } from "../../store/groups";
+import { useHistory } from "react-router-dom";
+
+// styles here
 import "./CreateGroupForm.css";
+
+const CreateGroupForm = () => {
+  const groups = useSelector((state) => state.groups);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [playStyle, setPlayStyle] = useState("");
+  const [description, setDescription] = useState("");
+  const [groupName, setGroupName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const updatePlayStyle = (e) => setPlayStyle(e.target.value);
+  const updateDescription = (e) => setDescription(e.target.value);
+  const updateGroupName = (e) => setGroupName(e.target.value);
+  const updateImageUrl = (e) => setImageUrl(e.target.value);
+
+  useEffect(() => {
+    dispatch(getGroups());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (pokeTypes.length && !type) {
+      setType(pokeTypes[0]);
+    }
+  }, [pokeTypes, type]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const payload = {
+      hostUserId: currentUser.id,
+      playStyle,
+      description,
+      groupName,
+      imageUrl,
+    };
+
+    let createdGroup = await dispatch(createGroup(payload));
+    if (createdGroup) {
+      history.push(`/groups/${createdGroup.id}`);
+      hideForm();
+    }
+  };
+
+  const handleCancelClick = (e) => {
+    e.preventDefault();
+    hideForm();
+  };
+
+  return (
+    <div className="newGroup-container">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="string"
+          placeholder="Name of Group"
+          required
+          value={groupName}
+          onChange={updateGroupName}
+        />
+        <input
+          type="string"
+          placeholder="Play Style"
+          required
+          value={playStyle}
+          onChange={updatePlayStyle}
+        />
+        <input
+          type="text"
+          placeholder="Description of group"
+          value={description}
+          onChange={updateDescription}
+        />
+        <input
+          type="string"
+          placeholder="Image URL"
+          value={imageUrl}
+          onChange={updateImageUrl}
+        />
+        <button type="submit">Create new Group</button>
+        <button type="button" onClick={handleCancelClick}>
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default CreateGroupForm;
