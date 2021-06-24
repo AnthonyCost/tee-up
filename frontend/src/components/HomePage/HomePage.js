30da0f5081aa4b48948e4ca2b3899a6f5a648978
Anthony Costanza <anthonycost99@gmail.com>
2021-06-23 23:32:27 -0700

    added home page route

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,1 +1,16 @@
 import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to Tee Up!</h1>
+      </div>
+      <div>
+        <img src="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
