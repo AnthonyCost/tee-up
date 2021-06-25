efe48f904bd9d08f5e8a18aa8b11d31e7bd55c37
Anthony Costanza <anthonycost99@gmail.com>
2021-06-25 08:25:58 -0700

    adding template for deleting a group

---
+++ frontend/src/DeleteGroupModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Modal } from "../../context/Modal";
+import DeleteGroupPrompt from "../DeleteGroupPrompt";
+import "./DeleteGroupModal.css";
+
+function DeleteGroupModal() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>Delete Group</button>
+      {showModal && (
+        <Modal onClose={() => setShowModal(false)}>
+          <DeleteGroupPrompt />
+        </Modal>
+      )}
+    </>
+  );
+}
+
+export default DeleteGroupModal;
