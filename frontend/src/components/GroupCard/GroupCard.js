29ce31f357be3719d9dd3e0d62cf10c09e99989f
Anthony Costanza <anthonycost99@gmail.com>
2021-06-25 09:32:18 -0700

    added optional chaining to groupCard

--- frontend/src/components/GroupCard/GroupCard.js
+++ frontend/src/components/GroupCard/GroupCard.js
@@ -9,20 +9,20 @@ const GroupCard = ({ group }) => {
       <div className="imageContainer">
         <img
           src={
-            group.imageUrl
+            group?.imageUrl
               ? group.imageUrl
               : "https://images.unsplash.com/photo-1612672916367-a185107c8aaa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80"
           }
         />
       </div>
       <div className="cardTitle">
-        <h2>{group.groupName}</h2>
+        <h2>{group?.groupName}</h2>
       </div>
       <div className="cardSubTitle">
-        <h3>Hosted by: {group.host.username}</h3>
+        <h3>Hosted by: {group?.host?.username}</h3>
       </div>
       <div className="cardDescription">
-        <p>{group.description}</p>
+        <p>{group?.description}</p>
       </div>
       <div className="cardBtn">
         <NavLink to={`/groups/${group.id}`}>
